1d58551f3c120ebc404be887fb4c9f75cddb0d62
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-09-22 15:43:53 +0200

    Sort testing added.

--- Session_1/testing/dataset_generator_test/_sort_test.py
+++ Session_1/testing/dataset_generator_test/_sort_test.py
@@ -3,3 +3,25 @@ ___author__ = 'acmASCIS'
 '''
     by ahani at {9/22/2016}
 '''
+
+import unittest
+from dataset_generator._sort import Sort
+
+
+class SortTest(unittest.TestCase):
+    def setUp(self):
+        self.__data_size = 4
+        self.__sort = Sort(self.__data_size)
+
+    def test_class_creation(self):
+        self.assertIsNotNone(self.__sort)
+
+    def test_array_creation(self):
+        self.assertIsNotNone(self.__sort.generate_data())
+
+    def test_array_size(self):
+        arr = self.__sort.generate_data()
+        self.assertEqual(len(arr), self.__data_size)
+
+if __name__ == '__main__':
+    unittest.main()
