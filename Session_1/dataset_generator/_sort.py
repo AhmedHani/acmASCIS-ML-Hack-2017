acd15f549a3e5312c05476c916218f9b97291007
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-09-22 14:53:04 +0200

    adding dataset generators files and implemented some of them.

---
+++ Session_1/dataset_generator/_sort.py
@@ -0,0 +1,26 @@
+___author__ = 'acmASCIS'
+
+'''
+    by ahani at {9/22/2016}
+'''
+
+import random
+
+
+class Sort(object):
+    def __init__(self, data_size):
+        self.__data_size = data_size
+        self.__array = []
+
+    def get_data_size(self):
+        return self.__data_size
+
+    def get_data(self):
+        return self.__array
+
+    def generate_data(self, min=0, max=10):
+        self.__array = [random.Random().randint(min, max) for i in range(self.__data_size)]
+
+        return self.__array
+
+
