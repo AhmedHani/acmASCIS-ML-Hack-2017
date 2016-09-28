beaa4b50d77079d260ae9783a250ea3b8bf0bae7
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-09-29 00:31:04 +0200

    Refactoring a function.

--- Session_1/release/submit.py
+++ Session_1/release/submit.py
@@ -11,14 +11,5 @@ Submit the chosen classes to the server.
 
 
 class Submit(object):
-    def __init__(self, project_dir):
-        self.__project_dir = project_dir
-
-    def send(self, sort_input_path, sort_output_path, freq_input_path, freq_output_path, matmul_input_path, matmul_output_path):
-        pass
-
-    def recv(self):
-        pass
-
-    def get_message(self):
+    def send(self, project_dir, sort_input_path, sort_output_path, freq_input_path, freq_output_path, matmul_input_path, matmul_output_path):
         pass
